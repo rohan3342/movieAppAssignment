af448a713b0cb80e010ac1858761be8f71193aaa
Rohan Kumar Singh <rosingh3342@gmail.com>
2021-04-06 13:45:42 +0530

    Initial Setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const App = () => {
+  return (
+    <View>
+      <Icon size={100} name="music" />
+    </View>
+  )
+}
+
+export default App;
